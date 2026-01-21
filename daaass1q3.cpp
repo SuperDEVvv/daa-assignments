2d0616838bf4145d879b0672cf3de9bd47c7e656
Dev <devtaneja18@gmail.com>
2026-01-22 01:42:35 +0530

    Add files via upload

---
+++ daaass1q3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int partitionArray(int a[], int low, int high) {
+    int pivot = a[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (a[j] <= pivot) {
+            i++;
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+
+    int temp = a[i + 1];
+    a[i + 1] = a[high];
+    a[high] = temp;
+
+    return i + 1;
+}
+
+void quickSort(int a[], int low, int high) {
+    if (low < high) {
+        int p = partitionArray(a, low, high);
+        quickSort(a, low, p - 1);
+        quickSort(a, p + 1, high);
+    }
+}
+
+int main() {
+    int arr[] = {4, 2, 6, 9, 2};
+    int n = 5;
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
